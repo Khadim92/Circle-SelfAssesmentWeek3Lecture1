b2dcf1080936b9e5eeb44fcb78b249e20ff63ee5
Khadim92 <imkhadim1@gmail.com>
2022-03-28 18:33:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 2;
+            circle c = new circle("Blue",2);
+            Console.WriteLine(c.radius);
+            Console.WriteLine(c.color);
+            circle c1 = new circle(c);
+            c1.color = "Green";
+            Console.WriteLine(c1.radius);
+            Console.WriteLine(c1.color);
+        }
+    }
+}
---
+++ circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class circle
+    {
+        public circle()
+        {
+            radius = 1.0f;
+            color="red";
+        }
+        public circle(int r)
+        {
+            radius = r;
+        }
+        public circle(string c, int r)
+        {
+            color = c;
+            radius = r;
+        }
+        public circle(circle c)
+        {
+            color = c.color;
+            radius = c.radius;
+        }
+        public float radius;
+        public string color;
+    }
+}
